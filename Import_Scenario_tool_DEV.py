d343fd79ffbd62406eea91b4c13ffdb17dc16908
Kaosrp <matheush963@gmail.com>
2025-02-19 10:51:42 -0300

    Update Import_Scenario_tool_DEV.py

--- Import_Scenario_tool_DEV.py
+++ Import_Scenario_tool_DEV.py
@@ -826,7 +826,7 @@ elif module_selected == "Simulador de Cenários":
             history.append(simulation_record)
             save_history(history)
             st.success("Simulação salva no histórico com sucesso!")
-            else:
+    else:
             st.warning("Nenhuma configuração encontrada para a filial selecionada. Verifique se há cenários com valores > 0 ou se a base de custos está configurada.")
          
     # MODO: COMPARAÇÃO MULTIFILIAL
