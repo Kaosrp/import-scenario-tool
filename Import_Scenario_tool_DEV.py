f84ff10ae05cc0cdd0dc8f7d204da27ad8f8dd6f
Kaosrp <matheush963@gmail.com>
2025-02-19 10:54:00 -0300

    Update Import_Scenario_tool_DEV.py

--- Import_Scenario_tool_DEV.py
+++ Import_Scenario_tool_DEV.py
@@ -830,7 +830,7 @@ elif module_selected == "Simulador de Cenários":
             st.warning("Nenhuma configuração encontrada para a filial selecionada. Verifique se há cenários com valores > 0 ou se a base de custos está configurada.")
          
     # MODO: COMPARAÇÃO MULTIFILIAL
-    #else:
+    else:
         st.subheader("Comparação Multifilial")
         if not data:
             st.warning("Nenhuma filial cadastrada. Adicione filiais na aba Gerenciamento.")
