4df73d1fea37bfd3ed82b4107f0be0507f9f947a
Kaosrp <matheush963@gmail.com>
2025-02-19 22:39:03 -0300

    Update Import_Scenario_tool_DEV.py

--- Import_Scenario_tool_DEV.py
+++ Import_Scenario_tool_DEV.py
@@ -16,8 +16,8 @@ st.markdown(
     .fixed-logo {
       position: fixed;
       top: 10px;
-      right: 10px;
-      width: 150px;
+      right: 15px;
+      width: 85px;
       z-index: 9999;
     }
     </style>
